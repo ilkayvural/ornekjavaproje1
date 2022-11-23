1804718d418c6159b184a4895cad07187b103733
İlkay Vural <95523420+ilkayvural@users.noreply.github.com>
2022-11-24 01:45:02 +0300

    OrnekJavaProje1
    bosluklaritemizle

---
+++ src/main/java/tr/edu/medipol/yova/orrnek2/BosluklariTemizle.java
@@ -0,0 +1,10 @@
+package tr.edu.medipol.yova.orrnek2;
+
+public class BosluklariTemizle {
+
+	public static void main(String[] args) {
+	String sonuc = StringUtils.deletewithspace(" abb hmm tyml trds vvv ");
+		System.out.println(sonuc);
+		}
+
+}
